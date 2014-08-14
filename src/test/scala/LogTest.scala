3903ce96620fa6710c16f27f7a5089d9681c82e4
Alexandru Matei <alexandru.matei@busymachines.com>
2014-08-14 16:25:42 +0300

    Direct logging in elasticsearch works. Useful for logging simple exceptions.

--- src/test/scala/LogTest.scala
+++ src/test/scala/LogTest.scala
@@ -2,7 +2,7 @@
 import org.apache.logging.log4j.LogManager
 import org.scalatest.FlatSpec
 /**
- * Created by alex on 13.08.2014.
+ * Created by Alexandru Matei on 13.08.2014.
  */
 class Bar {
    val logger = LogManager.getLogger();
@@ -16,7 +16,7 @@ class Bar {
 
 class LogTest extends FlatSpec {
 val logger= LogManager.getLogger()
-  "Log test" should "log by defaul" in {
+  "Log test" should "log by default" in {
     logger.trace("Starting test..")
     val bar = new Bar()
     if(!bar.doIt())
